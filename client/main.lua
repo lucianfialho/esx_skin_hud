931c3669c1869bcd9e5bb78e90e4c9d892e6fad8
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-07 15:00:00 -0300

    -- resolvendo a porra do problema de alteraçao de camera

--- client/main.lua
+++ client/main.lua
@@ -1,10 +1,92 @@
-local cam
-local zoomOffset, camOffset, heading, skinLoaded = 0.0, 0.0, 90.0, false
+local lastSkin, playerLoaded, cam, isCameraActive
+local zoomOffset, camOffset, heading, skinLoaded = true, 0.0, 0.0, 90.0, false
+
+
+function CreateSkinCam()
+    if not DoesCamExist(cam) then
+        cam = CreateCam('DEFAULT_SCRIPTED_CAMERA', true)
+    end
+
+    local playerPed = PlayerPedId()
+
+    SetCamActive(cam, true)
+    RenderScriptCams(true, true, 500, true, true)
+
+    isCameraActive = true
+    SetCamRot(cam, 0.0, 0.0, 270.0, true)
+    SetEntityHeading(playerPed, 0.0)
+end
+
+function loadCamera(camOffset, zoomOffset)
+    CreateSkinCam()
+
+    DisableControlAction(2, 30, true)
+    DisableControlAction(2, 31, true)
+    DisableControlAction(2, 32, true)
+    DisableControlAction(2, 33, true)
+    DisableControlAction(2, 34, true)
+    DisableControlAction(2, 35, true)
+    DisableControlAction(0, 25, true) -- Input Aim
+    DisableControlAction(0, 24, true) -- Input Attack
+
+    local angle = 90
+    
+    if isCameraActive then
+        if angle > 360 then
+            angle = angle - 360
+        elseif angle < 0 then
+            angle = angle + 360
+        end
+
+        heading = angle + 0.0
+    end
+
+    local playerPed = PlayerPedId()
+    local coords    = GetEntityCoords(playerPed)
+
+    local angle = heading * math.pi / 180.0
+    local theta = {
+        x = math.cos(angle),
+        y = math.sin(angle)
+    }
+
+
+    local pos = {
+        x = coords.x + (zoomOffset * theta.x),
+        y = coords.y + (zoomOffset * theta.y)
+    }
+
+
+    local angleToLook = heading - 140.0
+    if angleToLook > 360 then
+        angleToLook = angleToLook - 360
+    elseif angleToLook < 0 then
+        angleToLook = angleToLook + 360
+    end
+
+    angleToLook = angleToLook * math.pi / 180.0
+    local thetaToLook = {
+        x = math.cos(angleToLook),
+        y = math.sin(angleToLook)
+    }
+
+    local posToLook = {
+        x = coords.x + (zoomOffset * thetaToLook.x),
+        y = coords.y + (zoomOffset * thetaToLook.y)
+    }
+
+    cam = CreateCam('DEFAULT_SCRIPTED_CAMERA', true)
+
+    SetCamCoord(cam, pos.x, pos.y, coords.z + camOffset)
+    PointCamAtCoord(cam, posToLook.x, posToLook.y, coords.z + camOffset)
+
+end
 
 RegisterNetEvent('esx_skin_hud:OpenNui')
 AddEventHandler('esx_skin_hud:OpenNui', function()
 
     SetNuiFocus(false,false)
+    DeleteSkinCam()
 
     TriggerEvent('skinchanger:getSkin', function(skin)
         lastSkin = skin
@@ -45,96 +127,20 @@ AddEventHandler('esx_skin_hud:OpenNui', function()
             table.insert(elements, data)
         end
 
-        CreateSkinCam()
         zoomOffset = _components[1].zoomOffset
         camOffset = _components[1].camOffset
 
-        local playerPed = PlayerPedId()
-        local coords    = GetEntityCoords(playerPed)
-
-        local angle = heading * math.pi / 180.0
-
-        local theta = {
-            x = math.cos(angle),
-            y = math.sin(angle)
-        }
-
-        local pos = {
-            x = coords.x + (zoomOffset * theta.x),
-            y = coords.y + (zoomOffset * theta.y)
-        }
-
-        local angle = 90
-        local angleToLook = heading - 140.0
-
-        if angleToLook > 360 then
-            angleToLook = angleToLook - 360
-        elseif angleToLook < 0 then
-            angleToLook = angleToLook + 360
-        end
-
-        angleToLook = angleToLook * math.pi / 180.0
-
-        local thetaToLook = {
-            x = math.cos(angleToLook),
-            y = math.sin(angleToLook)
-        }
-
-        local posToLook = {
-            x = coords.x + (zoomOffset * thetaToLook.x),
-            y = coords.y + (zoomOffset * thetaToLook.y)
-        }
-
-        cam = CreateCam('DEFAULT_SCRIPTED_CAMERA', true)
-        
-
+        loadCamera(zoomOffset, camOffset)
         SetNuiFocus(true, true)
         SendNUIMessage({ showSkinHud = true, components = elements})
     end)
 end)
 
 
-RegisterNUICallback('esx_skin_hud:ChangeCamera', function(data, cb)
-    local coords    = GetEntityCoords(playerPed)
-
-    local angle = heading * math.pi / 180.0
-
-    local theta = {
-        x = math.cos(angle),
-        y = math.sin(angle)
-    }
-
-    local pos = {
-        x = coords.x + (zoomOffset * theta.x),
-        y = coords.y + (zoomOffset * theta.y)
-    }
-
-    local angle = 90
-    local angleToLook = heading - 140.0
-
-    if angleToLook > 360 then
-        angleToLook = angleToLook - 360
-    elseif angleToLook < 0 then
-        angleToLook = angleToLook + 360
-    end
-
-    angleToLook = angleToLook * math.pi / 180.0
-
-    local thetaToLook = {
-        x = math.cos(angleToLook),
-        y = math.sin(angleToLook)
-    }
-
-    local posToLook = {
-        x = coords.x + (zoomOffset * thetaToLook.x),
-        y = coords.y + (zoomOffset * thetaToLook.y)
-    }
+RegisterNUICallback('esx_skin_hud:ChangeOption', function(data, cb)
+    loadCamera(data.data.camOffset, data.data.zoomOffset)
     
-    cam = CreateCam('DEFAULT_SCRIPTED_CAMERA', true)
-
-    local camOffset = data.data
-
-    SetCamCoord(cam, pos.x, pos.y, coords.z + camOffset)
-    PointCamAtCoord(cam, posToLook.x, posToLook.y, coords.z + camOffset)
+    
+    
     cb('OK')
 end)
