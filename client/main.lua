b46698f3b61afde5d6aae4e9a42f304df53a90cd
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-06 23:22:46 -0300

    -- criando rota que muda a camera

--- client/main.lua
+++ client/main.lua
@@ -86,10 +86,55 @@ AddEventHandler('esx_skin_hud:OpenNui', function()
         }
 
         cam = CreateCam('DEFAULT_SCRIPTED_CAMERA', true)
-        SetCamCoord(cam, pos.x, pos.y, coords.z + camOffset)
-        PointCamAtCoord(cam, posToLook.x, posToLook.y, coords.z + camOffset)
+        
 
         SetNuiFocus(true, true)
         SendNUIMessage({ showSkinHud = true, components = elements})
     end)
+end)
+
+
+RegisterNUICallback('esx_skin_hud:ChangeCamera', function(data, cb)
+    local coords    = GetEntityCoords(playerPed)
+
+    local angle = heading * math.pi / 180.0
+
+    local theta = {
+        x = math.cos(angle),
+        y = math.sin(angle)
+    }
+
+    local pos = {
+        x = coords.x + (zoomOffset * theta.x),
+        y = coords.y + (zoomOffset * theta.y)
+    }
+
+    local angle = 90
+    local angleToLook = heading - 140.0
+
+    if angleToLook > 360 then
+        angleToLook = angleToLook - 360
+    elseif angleToLook < 0 then
+        angleToLook = angleToLook + 360
+    end
+
+    angleToLook = angleToLook * math.pi / 180.0
+
+    local thetaToLook = {
+        x = math.cos(angleToLook),
+        y = math.sin(angleToLook)
+    }
+
+    local posToLook = {
+        x = coords.x + (zoomOffset * thetaToLook.x),
+        y = coords.y + (zoomOffset * thetaToLook.y)
+    }
+    
+    cam = CreateCam('DEFAULT_SCRIPTED_CAMERA', true)
+
+    local camOffset = data.data
+
+    SetCamCoord(cam, pos.x, pos.y, coords.z + camOffset)
+    PointCamAtCoord(cam, posToLook.x, posToLook.y, coords.z + camOffset)
+    cb('OK')
 end)
